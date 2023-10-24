390d2bacb1303ccec786a63b001bcd369e2b9c14
mwsadler <45072532+mwsadler@users.noreply.github.com>
2023-10-23 22:50:10 -0400

    added redirect to heroes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import {HeroesComponent} from "./heroes/heroes.component";
 
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent }
+  { path: 'heroes', component: HeroesComponent },
+  {
+    path: '**',
+    redirectTo:'heroes'
+  },
 ];
 
 @NgModule({
